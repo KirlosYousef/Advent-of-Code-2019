ebc8879d778b5b938e2c861b634823bee0304b26
Kirlos Yousef <kirlosek@gmail.com>
2019-12-05 02:32:40 +0100

    Code comment, and README updated.

--- AOC-Day4/SecureContainer.py
+++ AOC-Day4/SecureContainer.py
@@ -18,16 +18,17 @@ def main():
                     flag = False
 
         if flag:
-            flag2 = 10
+
+            # If this number is prohibited because it has been repeated more than twice!
+            prohibited_num = 10     # 10 is the default value cause it's out of the range 0 to 9.
+
             for j in range(0, 5):
                 if j < 4 and pass_array[j] == pass_array[j+1] == pass_array[j+2]:
-                    flag2 = pass_array[j]
-                    pass
-                elif pass_array[j] == pass_array[j+1] and pass_array[j] != flag2:
+                    prohibited_num = pass_array[j]
+
+                elif pass_array[j] == pass_array[j+1] and pass_array[j] != prohibited_num:
                     passwords.append(pass_array)
                     break
-
-    # print(passwords)
 
     print('The number of different passwords within the range given:', len(passwords))
 
--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Collect stars by solving puzzles. Two puzzles will be made available on each day
 - [Day 1: The Tyranny of the Rocket Equation](AOC-Day1/)
 - [Day 2: 1202 Program Alarm](AOC-Day2/)
 - [Day 3: Crossed Wires](AOC-Day3/)
+- [Day 4: Secure Container](AOC-Day4/)
 
 ## Advent of Code 2019
 - http://adventofcode.com/2019
