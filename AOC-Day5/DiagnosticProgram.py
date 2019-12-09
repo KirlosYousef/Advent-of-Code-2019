3b6136dfc74be1047cab26f53ce896124de41ad3
Kirlos Yousef <kirlosek@gmail.com>
2019-12-09 04:07:50 +0100

    Day 5 - Part 2

--- AOC-Day5/DiagnosticProgram.py
+++ AOC-Day5/DiagnosticProgram.py
@@ -18,7 +18,10 @@ def int_code(numbers_array, input_val):
                 opcode = str(opcode)
                 if len(opcode) < 4:
                     for _ in range(4 - len(opcode)):
-                        opcode = '0' + opcode
+                        if int(opcode[-2:]) > 8:
+                            opcode = opcode[:-1] + '0' + opcode[-1]
+                        else:
+                            opcode = '0' + opcode
 
                 mode1 = int(opcode[1])
                 mode2 = int(opcode[0])
@@ -28,21 +31,48 @@ def int_code(numbers_array, input_val):
             if mode1 == 1:
                 first_ind = i + 1
 
-            if opcode == 1 or opcode == 2:
+            if opcode in [1, 2, 5, 6, 7, 8]:
                 second_ind = numbers_array[i + 2]
                 if mode2 == 1:
                     second_ind = i + 2
 
                 third_ind = numbers_array[i + 3]
+
                 if opcode == 1:
                     out_put = numbers_array[first_ind] + numbers_array[second_ind]
                     numbers_array[third_ind] = out_put
+                    i += 4
 
                 elif opcode == 2:
                     out_put = numbers_array[first_ind] * numbers_array[second_ind]
                     numbers_array[third_ind] = out_put
+                    i += 4
 
-                i += 4
+                elif opcode == 5:     # jump-if-true
+                    if numbers_array[first_ind] != 0:
+                        i = numbers_array[second_ind]
+                    else:
+                        i += 3
+
+                elif opcode == 6:     # jump-if-false
+                    if numbers_array[first_ind] == 0:
+                        i = numbers_array[second_ind]
+                    else:
+                        i += 3
+
+                elif opcode == 7:     # less than
+                    if numbers_array[first_ind] < numbers_array[second_ind]:
+                        numbers_array[third_ind] = 1
+                    else:
+                        numbers_array[third_ind] = 0
+                    i += 4
+
+                elif opcode == 8:     # equals
+                    if numbers_array[first_ind] == numbers_array[second_ind]:
+                        numbers_array[third_ind] = 1
+                    else:
+                        numbers_array[third_ind] = 0
+                    i += 4
 
             elif opcode == 3:
                 out_put = input_val
